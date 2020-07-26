ed8f5cb2e21b0bd510a2f8a698639bfa60a4a40d
Shivam Kumar <shivam.k@deltax.com>
2020-07-26 19:49:39 +0530

    changed prot from 8081 to 8080 --refs#11

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require("./config/db");
 const { MongoClient } = require("mongodb");
 const app = express();
 
-const port = 8081;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
